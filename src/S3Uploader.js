153d50e7f098edfb9127f54addc31ebb5c2f932a
Jose Garcia <j.edgji@gmail.com>
2016-10-07 20:26:58 -0700

    initial commit

---
+++ src/S3Uploader.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+class S3Uploader extends Component {
+
+}
+
+export default S3Uploader
---
+++ src/Upload.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import RcUpload from 'rc-upload/lib/Upload'
+
+const Upload = (props) => (
+  // <RcUpload {...props} />
+  <div></div>
+)
+
+export default Upload
